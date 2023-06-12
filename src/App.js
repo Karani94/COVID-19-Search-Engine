6fafa144fd3434f5b91949ea895f0e2fc71d4596
Muhammad Ormdv <muhammadoramedov94@gmail.com>
2023-06-12 22:57:40 +0300

    uploaded from local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const[veri,setVeri]=useState();
+  const[tarih,setTarih]=useState();
 
-export default App;
+  useEffect(()=>{
+    axios.get('https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json')
+    .then(res=>setVeri(res.data[tarih]))
+    .catch(err=>console.log(err))
+
+  },[veri,tarih])
+
+  return (
+    <div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 mx-auto mt-4'>
+            <h2 className='text-center text-white display-3'>Türkiye COVID-19 Search Engine</h2>
+            <input type='text' placeholder='GG/AA/YY' className='form-control' onChange={x=>setTarih(x.target.value)}/>
+            <table className='table text-white'>
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Number of tests</th>
+                  <th scope="col">Number of patients</th>
+                  <th scope="col">Number of Deaths</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className={veri===undefined ? 'bg-danger' : 'bg-success'}>
+                  <th scope="row">{veri===undefined ? 'Waiting for Data' : veri.date}</th>
+                  <td>{veri===undefined ? 'Waiting for Data' : veri.totalTests}</td>
+                  <td>{veri===undefined ? 'Waiting for Data' : veri.patients}</td>
+                  <td>{veri===undefined ? 'Waiting for Data' : veri.deaths}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App
